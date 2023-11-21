c47c4f6bf1178c750ae70fdbe837233c2217773d
2102kh <kholmatova403@gmail.com>
2023-11-21 16:14:47 +0100

    skapade en lista i JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,23 @@
 import "./../scss/style.css";
+
+let todosArr =[
+    {
+        id:1,
+        title:"Learn the JS function",
+        completed:false
+    },
+    {
+        id:2,
+        title:"Exersices with objekt",
+        completed:true
+    },
+    {
+        id:3,
+         title:"Learn about the Events in JS",
+         completed:false
+    }
+];
+const ul = document.querySelector("#todolist");
+const input = document.querySelector("#input");
+
+
