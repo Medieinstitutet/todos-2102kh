b51d390119b2cf7c4b88f2df323a3241450020fc
2102kh <kholmatova403@gmail.com>
2023-11-22 12:43:08 +0100

    work somewhat complete

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-import "./../scss/style.css";
+import "../scss/style.css";
 
 let todosArr =[
     {
@@ -13,11 +13,74 @@ let todosArr =[
     },
     {
         id:3,
-         title:"Learn about the Events in JS",
-         completed:false
+        title:"Learn about the Events in JS",
+        completed:false
     }
-];
-const ul = document.querySelector("#todolist");
-const input = document.querySelector("#input");
+  ];
+  const todos = document.querySelector("#todolist");
+  const input = document.querySelector(".input");
+  
+  //<li class="todo__list" data-id="1"></li>
+  
+  const createTodos = () => {
+    todos.innerHTML = '';
+    todosArr.forEach(element => {
+    const li = document.createElement("li");
+    li.className="todo__list";
+     if(element.completed) {
+      li.classList.add('todo__list_completed')
+      }else {
+      li.classList.remove('todo__list_completed')
+      }// skapade  lista med punkter med 
+  
+  
+      li.id = element.id;
+      li.textContent = element.title;
+      const button= document.createElement("button");
+      button.innerText="Ta bort";
+      li.append(button);
+  
+      button.addEventListener("click",()=>{
+      todosArr = todosArr.filter(item => item.id !== element.id);
+      createTodos(); 
+     })
+     
+     li.addEventListener("click",()=>{
+      
+      handleClick(element)
+     })
+     todos.append(li);
+   });
+  };
+  createTodos();
 
+  function handleClick(el){
+    todosArr.forEach(item => {
+      if(item.id === el.id){
+        console.log('item.id === el.id', item.id);
+        item.completed = !item.completed;
+        console.log('item.completed', item.completed);
+      }
+    })
+    createTodos();
+}
+input.addEventListener("keyup",(event)=>{
+    if(event.keyCode === 13){
+      const id = todosArr[todosArr.length -1]?.id + 1 || 1; // ||
+      
+      todosArr.push({
+        id: id,
+        title: input.value,
+        completed: false,
+      });
+      input.value = '';
+      createTodos();
+    }
+    });
+    
+  
+  
+  
+  
+  
 
